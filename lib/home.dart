d3294dc60c8877ae8dfd7aec7df753319a88d18e
Jazeel <avjazeel2003@gmail.com>
2024-01-27 12:34:21 +0530

    addedUpperNavigationTabs

--- lib/home.dart
+++ lib/home.dart
@@ -12,7 +12,7 @@ class HomeHome extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.network("https://scontent.fcok7-1.fna.fbcdn.net/v/t39.30808-6/336531854_1272644190327578_8404236082899569721_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=vVhLyBZSUPwAX-VdiSe&_nc_ht=scontent.fcok7-1.fna&oh=00_AfArTfXvnSXpSCrO5VkYOHIhBC316_rlK-7rVi9GyXdecA&oe=65B28D5B")
+            Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiAp35gtD50A8KtitAsQj5Ob8ZDHTcUz1udw&usqp=CAU")
           ],
         )),
     );
--- lib/screen.dart
+++ lib/screen.dart
@@ -3,45 +3,32 @@ import 'package:flutter_application_7/home.dart';
 import 'package:flutter_application_7/person.dart';
 import 'package:flutter_application_7/search.dart';
 
-class ScreenHome extends StatefulWidget {
-  const ScreenHome({super.key});
-
-  @override
-  State<ScreenHome> createState() => _ScreenHomeState();
-}
-
-int _currentSelectedIndex = 0; 
-
-class _ScreenHomeState extends State<ScreenHome> {
-
-  final _pages = [ 
-    HomeHome(),
-    SearchSearch(),
-    CountPage(),
-    
-  ];
+class ScreenHome extends StatelessWidget {
+  const ScreenHome({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[_currentSelectedIndex],
-    bottomNavigationBar: BottomNavigationBar(
-      currentIndex: _currentSelectedIndex,
-      onTap: (newIndex){
-        setState(() {
-          _currentSelectedIndex=newIndex;
-        });
-      },
-      selectedItemColor: Colors.blue,
-      unselectedItemColor: Colors.black,
-      items:const [
-      BottomNavigationBarItem(icon: Icon(Icons.home),label: 'home'),
-      BottomNavigationBarItem(icon: Icon(Icons.food_bank_rounded),label: 'menu'),
-      BottomNavigationBarItem(icon: Icon(Icons.book_rounded),label: 'order'),
-      
-      
-      
-    ]),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Canteen App'),
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.home), text: 'Home'),
+              Tab(icon: Icon(Icons.search), text: 'Search'),
+              Tab(icon: Icon(Icons.book), text: 'Orders'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            HomeHome(),
+            SearchSearch(),
+            CountPage(),
+          ],
+        ),
+      ),
     );
   }
 }
